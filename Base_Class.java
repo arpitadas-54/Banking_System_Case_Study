32d594fb5a52e18a4f3c7ec5d2d5e248bd81d6da
Arpita Das <dasarpita0369@gmail.com>
2026-06-21 18:29:35 +0530

    Create Base_Class.java

---
+++ Base_Class.java
@@ -0,0 +1,11 @@
+package Inheritance;
+
+public class Base_class {
+    int amt = 12345;
+    int balance = 23;
+
+    void display() {
+        System.out.println("Amount " + amt);
+        System.out.println("Balance " + balance);
+    }
+}
